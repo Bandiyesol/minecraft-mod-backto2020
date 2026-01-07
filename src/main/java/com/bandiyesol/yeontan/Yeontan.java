d0b111f59e5fd1b48ddab0fe08bd3e598bbb6cb0
DESKTOP-F5G7RUH\bandiyesol <yesolpark0909@gmail.com>
2026-01-07 21:55:01 +0900

    test 2

--- src/main/java/com/bandiyesol/yeontan/Yeontan.java
+++ src/main/java/com/bandiyesol/yeontan/Yeontan.java
@@ -22,7 +22,7 @@ public class Yeontan {
 
     public static final String MODID = "yeontan";
     public static final String NAME = "Back To 2020";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
 
     @Mod.Instance
     public static Yeontan instance;
